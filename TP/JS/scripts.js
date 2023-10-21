18b3d9461d20d3170f5e7d14650329473fe74e77
francolvega <francolvega@hotmail.com>
2023-10-21 09:44:25 -0300

    Update scripts.js

--- TP/JS/scripts.js
+++ TP/JS/scripts.js
@@ -18,11 +18,15 @@ formulario.addEventListener('submit', function(evento) {
 
     //validación de formulario:
     const {nombre, email, mensaje} = datos;
+    
 
-    if(nombre === '' || email === ''|| mensaje === ''  ) {
+    if(nombre === '' || email === '' || mensaje === ''  ) {
         mostrarAlerta('Todos los campos son obligatorios', true);
         return;
     }
+    validarNombre('nombre');
+    validarEmail('email');
+  
 
     //alerta de enviado con exito:
 
@@ -56,3 +60,54 @@ function mostrarAlerta(mensaje, error = null){
     }, 5000);
 
 }
+
+//funcion validar email
+
+function validarEmail(email){
+                
+	
+	var emailField = document.getElementById('email');
+	
+	// Definimos nuestras expresiones regulares
+	var validEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+
+	// Chequeamos si cumple
+	if( validEmail.test(emailField.value) ){
+		alert('Email valido, continue con el formulario');
+		return true;
+	}else{
+		alert('Email invalido, verifique su email');
+		return false;
+	}
+} 
+
+function validarNombre(nombre) {
+    for (var i = 0 ; i < nombre.length; i++) {
+        var charCode = nombre.charCodeAt(i);
+        if(!((charCode >= 65 && charCode <= 122) || charCode === 32)) {
+            alert("El campo 'nombre' solo puede contener caracteres alfábeticos y espacios.")
+            return false;
+        }
+    }
+}
+
+/*
+function validarFormulario() {
+    var nombre  = document.getElementById('nombre').value.trim();
+    var email   = document.getElementById('email').value.trim();
+    var mensaje = document.getElementById('mensaje').value.trim();
+
+    
+    if(nombre === '' || email === ''|| mensaje === ''  ) {
+        mostrarAlerta('Todos los campos son obligatorios', true);
+        return;
+    }
+
+    //alerta de enviado con exito:
+
+    mostrarAlerta('Mensaje enviado correctamente');
+
+    
+    }
+
+}*/
